aaf238471aa33516e5c1280cbd1d44e2d5b2e0b5
Sravas Melarkode <63852545+SupersonicCoder18@users.noreply.github.com>
2020-07-30 16:54:32 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDnVhvZsYFsHSQmbd0RehgpUzkpbSj9fFU",
+    authDomain: "book-santa-f1318.firebaseapp.com",
+    databaseURL: "https://book-santa-f1318.firebaseio.com",
+    projectId: "book-santa-f1318",
+    storageBucket: "book-santa-f1318.appspot.com",
+    messagingSenderId: "362198799749",
+    appId: "1:362198799749:web:10a73a5b6abb1e8bb460ec"
+  };
